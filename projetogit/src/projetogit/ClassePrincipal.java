e585f98c082b75c93d7f59decf16dc500ea7dbea
lab04 <lab04@lab4-11>
2024-04-25 22:31:24 -0300

    Adicionado IF

--- projetogit/src/projetogit/ClassePrincipal.java
+++ projetogit/src/projetogit/ClassePrincipal.java
@@ -5,6 +5,14 @@ public class ClassePrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		int idade = 18;
+		 if(idade > 18) {
+			 System.out.println("Okay");
+		 }
+		 else {
+			 System.out.println("No");
+		 }
 	}
 
 }
